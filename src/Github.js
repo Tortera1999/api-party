bc4176f401f89c9c0ff7b458ca34f5362d9f4841
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-05-29 11:10:09 -0400

    Add submit handler to update the url

--- src/Github.js
+++ src/Github.js
@@ -10,7 +10,12 @@ class Github extends Component {
   handleChange = (ev) => {
     this.setState({username: ev.target.value})
   }
-  
+
+  handleSubmit = (ev) => {
+      ev.preventDefault();
+      this.props.history.push(`/github/${this.state.username}`)
+      this.setState({username: ''})
+  }
   render() {
     return (
       <div className="Github">
@@ -19,7 +24,7 @@ class Github extends Component {
           src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
           alt="GitHub"
         />
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <input type="text" value={this.state.username} onChange={this.handleChange}/>
           </div>
